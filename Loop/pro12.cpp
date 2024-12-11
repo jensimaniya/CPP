9566036f00150632e2046eb8525fd42d2d6ee583
jensimaniya <jensimaniya@gmail.com>
2024-12-11 10:34:54 +0530

    LOOP

---
+++ Loop/pro12.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int i=1,n;
+
+    cout<<"enter any number :";
+    cin>>n;
+
+    while(i<25
+    n)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+}
