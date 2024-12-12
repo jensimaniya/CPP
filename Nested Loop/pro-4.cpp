b98ab3a2053fb5043e6a6bec6d5aa40ba638e099
jensimaniya <jensimaniya@gmail.com>
2024-12-12 10:43:53 +0530

    Nested Loop

---
+++ Nested Loop/pro-4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for( int i=1;i<=1;i++)
+    {
+        for(int j=1;j<=50;j++)
+        {
+            if(j%2==0)
+            {
+                cout<<j<<endl;
+            }
+        }
+    }
+
+
+
+    return 0;
+}
