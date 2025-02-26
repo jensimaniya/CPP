a1bec599d9330ca9643624f7a2d5ac0321e38666
jensimaniya <jensimaniya@gmail.com>
2025-02-26 16:42:24 +0530

    DSA

--- stack/pro1.cpp
+++ stack/pro1.cpp
@@ -1,20 +1,16 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
-int main()
-{
-    stack<int>stk;
+int main() {
+  stack<int> stk;
 
-    stk.push(10);
-    stk.push(20);
-    stk.push(30);
+  stk.push(10);
+  stk.push(20);
+  stk.push(30);
 
+  cout << stk.top() << endl;
+  cout << stk.size() << endl;
+  cout << stk.empty() << endl;
 
-    cout<<stk.top()<<endl;
-    cout<<stk.size()<<endl;
-    cout<<stk.empty()<<endl;
-
-    
-    return 0;
-
+  return 0;
 }
