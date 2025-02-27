c638d3f6f8d9cf3e0f605c736cc18e1f5c9f56c8
jensimaniya <jensimaniya@gmail.com>
2025-02-27 10:42:07 +0530

    stack

--- String/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
- if (substr.length() > 1 && substr[0] == substr[substr.length() - 1]) {
-    //     count++;
-    //   }
---
+++ stack/ReverseStack.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void insertAtBottom(stack<int> &s, int value) {
+  if (s.empty()) {
+    s.push(value);
+    return;
+  }
+  int top = s.top();
+  s.pop();
+  insertAtBottom(s, value);
+  s.push(top);
+}
+
+void print(stack<int> s) {
+  while (!s.empty()) {
+    cout << s.top() << endl;
+    s.pop();
+  }
+}
+
+void reverse(stack<int> &s) {
+  if (s.empty()) {
+    return;
+  }
+  int top = s.top();
+  s.pop();
+  reverse(s);
+  insertAtBottom(s, top);
+}
+
+int main() {
+  stack<int> s;
+  s.push(10);
+  s.push(20);
+  s.push(30);
+
+  insertAtBottom(s, 5);
+
+  print(s);
+cout<<"---------"<<endl;
+  
+  reverse(s);
+  print(s);
+  return 0;
+}
