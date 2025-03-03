5d5583be49ccc9b14194978fb74655224e07b93d
jensimaniya <jensimaniya@gmail.com>
2025-03-03 15:19:43 +0530

    cpp

--- Array/Two pointer/pro1.cpp
+++ Array/Two pointer/pro1.cpp
@@ -1,34 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int arr[5]={2,3,5,6,8};
-    for(int i=0; i<5; i++ )
-    {
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+int main() {
+  int arr[5] = {2, 3, 5, 6, 8};
+  for (int i = 0; i < 5; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
 
-    int i=0;
-    int j=4;
-    int temp;
+  int i = 0;
+  int j = 4;
+  int temp;
 
-    while(i<j){
-    temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
+  while (i < j) {
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 
     i++;
     j--;
-    }
+  }
 
-     for(int i=0; i<5; i++ )
-    {
-        cout<<arr[i]<<" ";
-    }  
+  for (int i = 0; i < 5; i++) {
+    cout << arr[i] << " ";
+  }
 
-
-
-
-    return 0;
+  return 0;
 }
