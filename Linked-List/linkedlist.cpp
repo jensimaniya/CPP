7152feb5a0e4acb9eb3a291c895e3822a5a1a1ff
jensimaniya <jensimaniya@gmail.com>
2025-03-19 10:23:55 +0530

    LinkedList

--- Linked-List/linkedlist.cpp
+++ Linked-List/linkedlist.cpp
@@ -1,54 +1,85 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
-    int data;
-    Node *next;
-    Node(int data){
-        this->data=data;
-        next=nullptr;
-    }
+class Node {
+public:
+  int data;
+  Node *next;
+  Node(int data) {
+    this->data = data;
+    next = nullptr;
+  }
 };
 
-class LinkedList{
-    public:
-    Node *head;
-    LinkedList(){
-        this->head=nullptr;
-    }
+class LinkedList {
+public:
+  Node *head;
+  LinkedList() { this->head = nullptr; }
 
-    void insertAtTail(int data){
-        Node *newNode= new Node(data);
-        if(head==nullptr){
-            head=newNode;
-            return;
-        }
-        Node *temp=head;
-        while(temp->next!=nullptr){
-            temp=temp->next;
-        }
-        temp->next=newNode;
+  void insertAtTail(int data) {
+    Node *newNode = new Node(data);
+    if (head == nullptr) {
+      head = newNode;
+      return;
     }
+    Node *temp = head;
+    while (temp->next != nullptr) {
+      temp = temp->next;
+    }
+    temp->next = newNode;
+  }
 
-    void PrintList(){
-        Node *temp=head;
-        while(temp){
-            cout<<temp->data<<" "<<"="<<" "<<temp->next<<" "<<"->"<<" ";
-            temp=temp->next;
-        }
+  void insertAthead(int value) {
+    Node *newNode = new Node(value);
+    if (head == nullptr) {
+      head = newNode;
+      return;
     }
+    newNode->next = head;
+    head = newNode;
+  }
+  void Addone() {
+    Node *temp = head;
+    while (temp) {
+      temp->data = temp->data + 1;
+      temp = temp->next;
+    }
+  }
+  void removeAtTail(){
+    Node *temp=head;
+    while(temp->next->next){
+        temp=temp->next;
+    }
+    temp->next=nullptr;
+  }
+
+  void PrintList() {
+    Node *temp = head;
+    while (temp) {
+      cout << temp->data << " "
+           << "->"
+           << " ";
+      temp = temp->next;
+    }
+  }
 };
 
+int main() {
+  LinkedList *list = new LinkedList();
+  list->insertAtTail(10);
+  list->insertAtTail(20);
+  list->insertAtTail(30);
+  list->insertAtTail(40);
+  list->insertAthead(5);
+  list->insertAthead(1);
+  list->PrintList();
 
-int main(){
-    LinkedList *list=new LinkedList();
-    list->insertAtTail(10);
-    list->insertAtTail(20);
-    list->insertAtTail(30);
-    list->insertAtTail(40);
+  cout << endl;
+  list->Addone();
+  list->PrintList();
+  list->removeAtTail();
+  cout<<endl;
+  list->PrintList();
 
-    list->PrintList();
-
-        return 0;
+  return 0;
 }
